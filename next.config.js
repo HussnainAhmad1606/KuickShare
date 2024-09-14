92168d6175251fd4b44a36289654fa3502698411
Hussnain Ahmad <75199596+HussnainAhmad1606@users.noreply.github.com>
2024-09-14 22:45:21 +0500

    change next.config.js

---
+++ next.config.js
@@ -0,0 +1,13 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {};
+
+// export default nextConfig;
+module.exports = {
+    typescript: {
+      ignoreBuildErrors: true,
+    },
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
+  };
+     
